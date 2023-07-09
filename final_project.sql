8483e6a7f45e1acc216338fc217912e8ba9b88d8
Anurag Saini <anuragsaini674@gmail.com>
2023-07-09 12:42:04 +0530

    Creation of one zip file

---
+++ final_project.sql
@@ -0,0 +1,13 @@
+Create database final_project;
+Use final_project;
+Create table booked(Class varchar(20),
+Ticket_Type varchar(20),
+Adult varchar(20),
+Child varchar(20),
+Fr_om varchar(20),
+T_o varchar(20),
+Da_te varchar(20),
+Ti_me varchar(20),
+Ticket_No varchar(10),
+Price varchar(10),
+Route varchar(10));
